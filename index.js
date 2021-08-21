5d800226e23a18864bee5fa4c9c14357352afad8
Leandro Sales <leandrossales.96@gmail.com>
2021-08-21 12:11:10 -0300

    Add bot check moodle status

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
+const Discord = require("discord.js");
+const isReachable = require("is-reachable");
+const config = require("./config.json");
 
-const client = new Discord.Client({intents:["GUILDS", "GUILD_MESSAGES"]});
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-client.login(`${config.token}`)
+async function checkStatus() {
+  let status = await isReachable(
+    "https://www.dcc.ufrrj.br/moodle/login/index.php"
+  );
+
+  if (status === true) {
+    client.user.setActivity("Moodle ON");
+    client.user.setPresence({
+      status: "online",
+    });
+  } else {
+    client.user.setActivity("Moodle OFF");
+    client.user.setPresence({
+      status: "dnd",
+    });
+  }
+}
+
+setInterval(checkStatus, 30000);
+
+client.login(`${config.token}`);
