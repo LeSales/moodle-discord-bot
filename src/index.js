d6fbd14b183415f2c916edc44e643d94089bb47d
Leandro Sales <leandrossales.96@gmail.com>
2021-08-31 18:06:12 -0300

    url adjust

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js");
 const isReachable = require("is-reachable");
-const config = require("./config.json");
+const config = require("../config.json");
+const url = require("./url");
+
+const serviceCheckTime = 30000;
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 async function checkStatus() {
-  let status = await isReachable(config.url);
+  let status = await isReachable(url.sigaa);
 
   if (status === true) {
     client.user.setActivity("Moodle ON");
@@ -20,6 +23,6 @@ async function checkStatus() {
   }
 }
 
-setInterval(checkStatus, 30000);
+setInterval(checkStatus, serviceCheckTime);
 
 client.login(`${config.token}`);
---
+++ src/url.js
@@ -0,0 +1,4 @@
+const moodle = "https://www.dcc.ufrrj.br/moodle/login/index.php";
+const sigaa = "https://sigaa.ufrrj.br/sigaa/verTelaLogin.do";
+
+module.exports = {moodle, sigaa};
