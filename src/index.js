68415fa296c053d5efbd82a860c9297b94d15cd4
Leandro Sales <leandrossales.96@gmail.com>
2021-09-02 13:23:50 -0300

    set to be online full time

--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ const url = require("./url");
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 async function checkStatus() {
-  let status = await isReachable(url.sigaa);
+  let status = await isReachable(url.moodle);
 
   if (status === true) {
     client.user.setActivity("Moodle ON");
